b67100feb54fe82fc0768d02e534a266683e14db
1dynamite <turaevshokhrukh7@gmail.com>
2022-07-13 03:07:27 +0500

    feat: use 3001 in case of no provision

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = require("./app");
 const URL =
   "mongodb+srv://shoxa98:V-_V9u-6*2X6bF%40@cluster0.cr3ur.mongodb.net/smarthouse?retryWrites=true&w=majority";
 
+const port = process.env.PORT || 3001;
 mongoose
   .connect(URL)
   .catch(() => console.log(`unable to connect to database: ${URL}`));
@@ -12,8 +13,6 @@ mongoose.connection.on("error", () => {
   console.log("database connection error");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Application is running on http://ip-address:${process.env.PORT}/`
-  );
+app.listen(port, () => {
+  console.log(`Application is running on http://ip-address:${port}/`);
 });
